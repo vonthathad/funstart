691a8b8ef1d2acdd963940926fe4af5f6e356fa1
andh <anic.dh@gmail.com>
2016-09-19 10:20:38 +0700

    fixed untracked files

--- public/sources/game/1/test.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by andh on 8/9/16.
- */
--- public/sources/game/2/js/application.js
+++
@@ -1,6 +0,0 @@
-// Wait till the browser is ready to render the game (avoids glitches)
-fsGame.gameStart(function () {
-  window.requestAnimationFrame(function () {
-    new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
-  });
-})
