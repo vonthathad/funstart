d45fe2ae120fcd2e04d2945470b1416eb55d4893
andh <anic.dh@gmail.com>
2016-09-22 09:38:48 +0700

    test socket

--- app/config/socket.js
+++ app/config/socket.js
@@ -11,8 +11,15 @@
 //    connections : connections,
 //    io : io
 //}
-var io = require('socket.io')(8888);
-console.log('io',io);
+// var io = require('socket.io')(8888);
+// console.log('io',io);
+// exports.getSocket = function(){
+//     return io;
+// };
+
+var app = require('http').createServer();
+var io = require('socket.io')(app);
+app.listen(8080);
 exports.getSocket = function(){
     return io;
 };
