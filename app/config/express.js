a94ff1790406832c6ebcbc923c3aa67ba70b5760
andh <anic.dh@gmail.com>
2016-09-27 11:19:02 +0700

    sua port

--- app/config/express.js
+++ app/config/express.js
@@ -51,7 +51,7 @@ module.exports = function() {
     require('../routes/index')(secure);
     app.use('/', secure);
 
-    app.set('port', (process.env.PORT || 8235));
+    app.set('port', (process.env.PORT || 8236));
 
     app.listen(app.get('port'), function() {
         console.log('Node app is running on port', app.get('port'));
