98b05a3a4a240bcedbbcbcf0af958abc877a4634
andh <anic.dh@gmail.com>
2016-10-06 12:24:44 +0700

    ignore bot

--- app/config/socket.js
+++ app/config/socket.js
@@ -1,5 +1,27 @@
-var io = require('socket.io')(8088);
-console.log('io',io);
+var express = require('express');
+var ioApp = express();
+var socket = require('socket.io');
+var LEX = require('letsencrypt-express').testing();
+var https = require('http2');
+var lex = LEX.create({
+    configDir: '/etc/letsencrypt'
+    , letsencrypt: null
+    , approveRegistration: function (hostname, cb) {
+        cb(null, {
+            domains: ['www.funstart.net']
+            ,email: 'secret.andoan@gmail.com'
+            ,agreeTos: true
+        });
+    }
+});
+
+var server = https.createServer(lex.httpsOptions, LEX.createAcmeResponder(lex, ioApp));
+server.listen(443);
+
+var io = socket.listen(server);
+
+// var io = require('socket.io')(8088);
+// console.log('io',io);
 exports.getSocket = function(){
     return io;
 };
