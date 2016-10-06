cd1b1ee2407e0a76b86fa2fa1a7e97310c0d0531
andh <anic.dh@gmail.com>
2016-10-06 14:05:23 +0700

    socket zz

--- app/config/socket.js
+++ app/config/socket.js
@@ -9,7 +9,17 @@
 //
 // var io = socket.listen(server);
 
-var io = require('socket.io')(8088);
+// var fs = require( 'fs' );
+var appIO = require('express')();
+var https        = require('https');
+var server = https.createServer({
+    requestCert: false,
+    rejectUnauthorized: false
+},appIO);
+server.listen(8080);
+var io = require('socket.io').listen(server);
+
+// var io = require('socket.io')(8088);
 // console.log('io',io);
 exports.getSocket = function(){
     return io;
