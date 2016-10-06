7410736ca78ba8c5f84726ff74a13acf3861cb44
andh <anic.dh@gmail.com>
2016-10-06 13:50:49 +0700

    tao socket https

--- app/config/socket.js
+++ app/config/socket.js
@@ -1,22 +1,11 @@
-var express = require('express');
-var ioApp = express();
 var socket = require('socket.io');
-var LEX = require('letsencrypt-express');
-var https = require('http2');
-var lex = LEX.create({
-    configDir: '/etc/letsencrypt'
-    , letsencrypt: null
-    , approveRegistration: function (hostname, cb) {
-        cb(null, {
-            domains: ['www.funstart.net']
-            ,email: 'secret.andoan@gmail.com'
-            ,agreeTos: true
-        });
-    }
-});
-
-var server = https.createServer(lex.httpsOptions, LEX.createAcmeResponder(lex, ioApp));
-server.listen(443);
+var server = require('letsencrypt-express').create({
+    server: 'staging'
+    , email: 'secret.andoan@gmail.com'
+    , agreeTos: true
+    , approveDomains: [ 'www.funstart.net' ]
+    , app: require('express')()
+}).listen(8088,443);
 
 var io = socket.listen(server);
 
