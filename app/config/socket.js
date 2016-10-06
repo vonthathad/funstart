99b90c6897eab01843c4acee59ef3a3b66ae8ce3
andh <anic.dh@gmail.com>
2016-10-06 13:58:36 +0700

    sua socket

--- app/config/socket.js
+++ app/config/socket.js
@@ -1,15 +1,15 @@
-var socket = require('socket.io');
-var server = require('letsencrypt-express').create({
-    server: 'staging'
-    , email: 'secret.andoan@gmail.com'
-    , agreeTos: true
-    , approveDomains: [ 'www.funstart.net' ]
-    , app: require('express')()
-}).listen(8088,443);
+// var socket = require('socket.io');
+// var server = require('letsencrypt-express').create({
+//     server: 'staging'
+//     , email: 'secret.andoan@gmail.com'
+//     , agreeTos: true
+//     , approveDomains: [ 'www.funstart.net' ]
+//     , app: require('express')()
+// }).listen(8088,443);
+//
+// var io = socket.listen(server);
 
-var io = socket.listen(server);
-
-// var io = require('socket.io')(8088);
+var io = require('socket.io')(8088);
 // console.log('io',io);
 exports.getSocket = function(){
     return io;
