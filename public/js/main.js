5888f3d1be5fda61df075649d8069194af7ac556
andh <anic.dh@gmail.com>
2016-10-14 00:45:50 +0700

    sua css cai ne

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,4 @@
-var loadDeferredStyles = function() {
-    var addStylesNode = document.getElementById("deferred-styles");
-    var replacement = document.createElement("div");
-    replacement.innerHTML = addStylesNode.textContent;
-    document.body.appendChild(replacement)
-    addStylesNode.parentElement.removeChild(addStylesNode);
-};
-var raf = requestAnimationFrame || mozRequestAnimationFrame ||
-    webkitRequestAnimationFrame || msRequestAnimationFrame;
-if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
-else window.addEventListener('load', loadDeferredStyles);
+
 var socket = io('//www.funstart.net:8088',{'forceNew':true});
 $(document).ready(function () {
     if (window.location.href.indexOf('#_=_') > 0) {
