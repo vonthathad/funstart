976f87c66360afc452663004d7924543668f514a
andh <anic.dh@gmail.com>
2016-10-14 00:43:19 +0700

    sua css xem sao

--- public/js/main.js
+++ public/js/main.js
@@ -5,9 +5,10 @@ var loadDeferredStyles = function() {
     document.body.appendChild(replacement)
     addStylesNode.parentElement.removeChild(addStylesNode);
 };
-angular.element(document).ready(function() {
-    loadDeferredStyles();
-});
+var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+    webkitRequestAnimationFrame || msRequestAnimationFrame;
+if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
+else window.addEventListener('load', loadDeferredStyles);
 var socket = io('//www.funstart.net:8088',{'forceNew':true});
 $(document).ready(function () {
     if (window.location.href.indexOf('#_=_') > 0) {
