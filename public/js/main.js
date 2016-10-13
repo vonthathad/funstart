dbcf1bc8d7f6d5d70322bbacda3b4cc7ae851712
andh <anic.dh@gmail.com>
2016-10-13 14:51:39 +0700

    sua file socket

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-var socket = io('//localhost:8088',{'forceNew':true});
+var socket = io('//www.funstart.net:8088',{'forceNew':true});
 $(document).ready(function () {
     if (window.location.href.indexOf('#_=_') > 0) {
         window.location = window.location.href.replace(/#.*/, '');
