0711f5dc779944be9a813e9d15632a9316df59e5
andh <anic.dh@gmail.com>
2016-10-24 15:22:53 +0700

    thu sua dom

--- app/controllers/upload.server.controller.js
+++ app/controllers/upload.server.controller.js
@@ -95,10 +95,10 @@ exports.captureResult = function(req,res){
                         '</div>'+
                         '<div style="position: absolute;left: 35%;width: calc(65% - 20px);height: calc(100% - 40px);padding: 20px 20px 20px 0;">';
         if(req.user.displayName){
-            domHTML+=       '<p style="padding: 10px;background: rgba(0,0,0,0.1);height: 80px">'+req.user.displayName+'</p>';
+            domHTML+=       '<p style="width: 100%;padding: 10px;background: rgba(0,0,0,0.1);height: 80px">'+req.user.displayName+'</p>';
         }
         domHTML+=
-                            '<p style="padding: 10px;background: rgba(0,0,0,0.1)">'+result.title+'</p>' +
+                            '<p style="width: 100%;padding: 10px;background: rgba(0,0,0,0.1)">'+result.title+'</p>' +
                         '</div>';
     }
     domHTML+=
