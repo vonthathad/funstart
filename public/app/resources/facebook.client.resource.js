f9d3216eaba355bc0a5ba28b2d2be4df0a3f14ed
andh <anic.dh@gmail.com>
2016-11-17 14:36:10 +0700

    fix bug

--- public/app/resources/facebook.client.resource.js
+++ public/app/resources/facebook.client.resource.js
@@ -10,10 +10,6 @@ angular.module('funstart').factory('$FB', ['$window', function($window) {
                         status: true,
                         xfbml: true
                     });
-                    if(document.getElementById('facebook-like')){
-                        console.log('TH1');
-                        document.getElementById('facebook-like').innerHTML = '<div class="fb-page" data-href="https://www.facebook.com/FunStart/" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/FunStart/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/FunStart/">Fun Start</a></blockquote></div>';
-                    }
                 };
                 (function(d) {
                     var js,
