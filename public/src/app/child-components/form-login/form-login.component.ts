a1e3789de5bf126b87d5d94dcb3b5c969f3e1380
thanhdatvo <thanhdat.vo.email@gmail.com>
2017-01-17 10:05:48 +0700

    add login by facebook and twitter

--- public/src/app/child-components/form-login/form-login.component.ts
+++ public/src/app/child-components/form-login/form-login.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-login.component.scss']
 })
 export class FormLoginComponent implements OnInit {
-
+  private location: string;
   constructor() { }
 
   ngOnInit() {
+    this.location = window.location.href;
+    console.log("Location: " + this.location);
   }
-
+  loginFacebook() {
+    window.location.href = `http://localhost:8235/oauth/facebook?redirect=${this.location}`;
+  }
+  loginTwitter() {
+    window.location.href = `http://localhost:8235/oauth/twitter?redirect=${this.location}`;
+  }
 }
