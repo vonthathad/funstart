a6859d204e9930ba7e06e9e8dc7ba501c3f29681
andh <anic.dh@gmail.com>
2017-01-25 15:41:04 +0700

    up new code to server

--- public/src/app/components-child/facebook-comment/facebook-comment.component.ts
+++ public/src/app/components-child/facebook-comment/facebook-comment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ElementRef, Input } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 @Component({
   selector: 'app-facebook-comment',
   templateUrl: 'facebook-comment.component.html',
@@ -6,16 +7,20 @@ import { Component, OnInit, ElementRef, Input } from '@angular/core';
 })
 export class FacebookCommentComponent implements OnInit {
   // @Input() url: string;
-  constructor(private el: ElementRef) { }
+  constructor(private route: Router, private el: ElementRef) { }
 
   ngOnInit() {
-    let commentsDom = (<HTMLElement>this.el.nativeElement).querySelector('#fb-comments') as HTMLElement;
-    commentsDom.innerHTML = '<div class="fb-comments" ' +
-        'data-href="' + location.href.split('?') + '" ' +
-        'data-width="' + commentsDom.clientWidth + '" ' +
-        'data-numposts="5" ' +
-        '</div>';
+      this.route.events.subscribe(event => {
+          if (event instanceof NavigationStart) {
+              let commentsDom = (<HTMLElement>this.el.nativeElement).querySelector('#fb-comments') as HTMLElement;
+              commentsDom.innerHTML = '<div class="fb-comments" ' +
+                  'data-href="' + event.url + '" ' +
+                  'data-width="' + commentsDom.clientWidth + '" ' +
+                  'data-numposts="5" ' +
+                  '</div>';
 
+          }
+      });
   }
 
 }
