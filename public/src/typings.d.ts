acd0e82f07845f412dad3b9f4f23263fad56a0c1
andh <anic.dh@gmail.com>
2017-01-27 00:16:07 +0700

    up new code to server

--- public/src/typings.d.ts
+++ public/src/typings.d.ts
@@ -1,4 +1,4 @@
 // Typings reference file, you can add your own global typings here
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
-interface Window { game: any, local: any, channel: any}
+interface Window { game: any, local: any, channel: any, FB: any}
 declare var google: any;
