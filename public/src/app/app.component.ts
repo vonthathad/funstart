d6845cb58306465c9dfa8bbdae0ec9d63a30b403
thanhdatvo <thanhdat.vo.email@gmail.com>
2017-02-03 11:40:20 +0700

    add angulartics2

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import {TranslateService} from "ng2-translate/index";
 import {FacebookService, FacebookInitParams} from 'ng2-facebook-sdk';
+import { Angulartics2GoogleAnalytics } from 'angulartics2';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private translate: TranslateService, private facebookService: FacebookService) {
+  constructor(private translate: TranslateService, private facebookService: FacebookService, angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) {
     translate.addLangs(["en", "fr"]);
     translate.setDefaultLang('en');
     let browserLang: string = translate.getBrowserLang();
