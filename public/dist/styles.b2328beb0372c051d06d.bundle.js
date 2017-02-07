6aa49e790974f21959a1908e63fd3d3efe86daeb
andh <anic.dh@gmail.com>
2017-02-07 08:38:46 +0700

    up new code to server

--- public/dist/styles.b2328beb0372c051d06d.bundle.js
+++ public/dist/styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{1115:function(n,o,c){n.exports=c(497)},497:function(n,o){}},[1115]);
+webpackJsonp([1,2],{1118:function(n,o,c){n.exports=c(498)},498:function(n,o){}},[1118]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
