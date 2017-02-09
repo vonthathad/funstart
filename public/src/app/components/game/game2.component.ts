410fb07893c4170a367ce6becb6793c2d0f4a53a
andh <anic.dh@gmail.com>
2017-02-09 17:26:16 +0700

    up new code to server

--- public/src/app/components/game/game2.component.ts
+++ public/src/app/components/game/game2.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { GameService } from '../../services/game.service'
 
+import { GameService } from '../../services/game.service'
 
 import { Game } from '../../classes/game';
 @Component({
@@ -9,34 +9,52 @@ import { Game } from '../../classes/game';
   templateUrl: './game2.component.html',
   styleUrls: ['./game.component.scss'],
 })
-export class Game2Component implements OnInit {
-  private game: Game;
-
-
+export class TopicComponent implements OnInit {
+  private topicId: string;
+  private games: Game[];
+  private paging: number;
+  private page: number;
+  private order: string;
+  private hasMore: boolean;
+  private isLoading: boolean;
   constructor(private route: ActivatedRoute, private gameService: GameService) {
+    this.paging = 8;
+    this.page = 1;
+    this.isLoading = true;
+    this.hasMore = true;
+    this.order = "new";
     route.params.subscribe(params => {
-      // this.gameRecommendBottomComponent.loadRecommendGame();
-      // this.gameRecommendLeftComponent.loadRecommendGame();
-      // this.gameRecommendRightComponent.loadRecommendGame();
-      gameService.getGame(params['id']).subscribe((res: any) => {
-        // console.log("IN PUT " + JSON.stringify(res.data));
-        gameService.gameSource.next(res.data);
-        this.game = res.data;
-        // this.gameIntroComponent.setVisible(true);
-        // this.iframeGameComponent.setVisible(false);
-        // this.gameShareComponent.setVisible(false);
-        // this.gameIntroComponent.setLibrariesPreloadDone(false);
-
-        // this.facebookCommentShowed = false;
-        // this.facebookCommentShowed = true;
-        // this.iframeAdsComponent._closeAds();
-      });
+      this.topicId = params['id'];
+      this.gameService.getGames({ topic: this.topicId, order: this.order, paging: this.paging, page: this.page }).subscribe((res: any) => this.renderGames(res.data,true,res.isNext))
     });
+
   }
 
   ngOnInit() {
-
-
   }
+  getGames(order) {
+    this.hasMore = true;
+    this.page = 1;
+    this.order = order;
+    this.gameService.getGames({ topic: this.topicId, order: this.order, paging: this.paging, page: this.page }).subscribe((res: any) => this.renderGames(res.data,true,res.isNext))
+  }
+  onScroll(){
 
+    if(this.hasMore && !this.isLoading){
+      this.isLoading = true;
+      this.page++;
+      this.gameService
+          .getGames({ topic: this.topicId, order: this.order, paging: this.paging, page: this.page})
+          .subscribe((res: any) => this.renderGames(res.data,false,res.isNext));
+    }
+  }
+  renderGames(games: Game[],isNew,isNext) {
+    if(isNew){
+      this.games = games;
+    } else {
+      this.games = this.games.concat(games);
+    }
+    this.hasMore = isNext;
+    this.isLoading = false;
+  }
 }
